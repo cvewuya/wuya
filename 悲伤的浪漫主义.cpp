bbaeca83d8509a3abfbb881271a619dfd1766144
cvewuya <138797178+cvewuya@users.noreply.github.com>
2023-07-07 13:56:29 +0300

    Add files via upload

---
+++ 悲伤的浪漫主义.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+	
+int main(){
+	/* 
+	================================================
+	|    ssssssss        A           DDDDDD        |
+	|    s              A A          D     D       |   
+	|    s             A   A         D      D      |
+	|    ssssssss     AAAAAAAA       D      D      |
+	|           s    A        A      D      D      |
+	|           s   A          A     D     D       |
+    |    ssssssss  A            A    DDDDDD        |
+	|                                              |
+    ================================================
+	程序设计和一本书一样，只有进入深度思考的时候才能发现浪漫QAQ
+	*/
+	using namespace std;
+	
+	string My_Sad = "我无法靠近你";                   //定义一个你喜欢的对象QWQ
+	
+	cout << My_sad;
+	
+	return 0; 
+
+
+}
+
